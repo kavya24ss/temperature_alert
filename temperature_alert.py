e91e9c80dfc88b2850046338133585d2f77e4565
kavya24ss <sagikavyasri@gmail.com>
2025-11-27 13:35:13 +0530

    Update temperature_alert.py

--- temperature_alert.py
+++ temperature_alert.py
@@ -1,12 +1,14 @@
 import sys
+
 if len(sys.argv) == 2:
     script_name = sys.argv[0]
     temp = sys.argv[1]
 else:
     temp = 25
+    
 if temp > 40:
     print("hot")
 elif temp > 20:
-    print("normal")
+    print("Normal")
 else:
     print("cold")

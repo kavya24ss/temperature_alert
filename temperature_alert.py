75eb0b22decb51cb76cc6cbf9ee7afbe9a381e4b
kavya24ss <sagikavyasri@gmail.com>
2025-11-27 13:42:03 +0530

    Update temperature_alert.py

--- temperature_alert.py
+++ temperature_alert.py
@@ -1,11 +1,14 @@
 import sys
 
 if len(sys.argv) == 2:
-    script_name = sys.argv[0]
-    temp = sys.argv[1]
+    try:
+        temp = int(sys.argv[1])
+    except ValueError:
+        print(f"Invalid temperature value: {sys.argv[1]}")
+        sys.exit(1)
 else:
     temp = 25
-    
+
 if temp > 40:
     print("hot")
 elif temp > 20:
